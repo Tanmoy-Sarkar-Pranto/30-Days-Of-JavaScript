40526ec131ee9174e973106cbfbdc14ece23fb72
Tanmoy-Sarkar-Pranto <ptanmoysarkar@gmail.com>
2023-08-04 21:19:17 +0600

    commit 2

---
+++ Solutions/Day 02/level1.js
@@ -0,0 +1,40 @@
+
+// console.log(challenge)
+// console.log(challenge.length)
+// console.log(challenge.toUpperCase())
+// console.log(challenge.toLowerCase())
+// console.log(challenge.substring(0,2))
+// console.log(challenge.substring(2,challenge.length))
+// console.log(challenge.includes("Script"))
+// console.log(challenge.split(' '))
+
+let newString = "Facebook,Google,Microsoft,Apple,IBM,Oracle,Amazon"
+// console.log(newString.split(','))
+
+// console.log(challenge.replace("JavaScript","Python"))
+// console.log(challenge)
+
+// console.log(challenge.charAt(15))
+// console.log(challenge.charCodeAt(challenge.indexOf('J')))
+
+let challenge = "30 Days Of JavaScript"
+// console.log(challenge.indexOf("a"))
+
+// console.log(challenge.lastIndexOf("a"))
+
+let string = "You cannot end a sentence with because because because is a conjunction"
+
+// console.log(string.indexOf("because"))
+// console.log(string.lastIndexOf("because"))
+// console.log(string.search("because"))
+
+// console.log(string.trim())
+// console.log(challenge.startsWith("30"))
+// console.log(challenge.endsWith("JavaScript"))
+
+// console.log(challenge.match('a'))
+let string1 = "30 Days of"
+let string2 = "JavaScript"
+// console.log(string1.concat(" ".concat(string2)))
+
+console.log(challenge.repeat(2))
---
+++ Solutions/Day 02/level2.js
@@ -0,0 +1,2 @@
+let quote = "There is no exercise better for the heart than reaching down and lifting people up."
+console.log(`The quote '${quote}' by John Holmes teaches us to help one another.`)
